0bda23a86433ac8cf2a7f88b090f51c4cebb229a
HallexCosta <hallex.costa@hotmail.com>
2022-08-08 02:48:28 -0300

    chore: adjust console.log and build

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import OctoController from './controllers/octoController'
 const GH_AUTH = process.env.GH_AUTH as string
 const GH_LOGIN = process.env.GH_LOGIN as string
 
+console.log('GH_LOGIN', GH_LOGIN)
 console.log('GH_AUTH', GH_AUTH)
+console.log()
 
   ; (async () => {
     const octoController = new OctoController(GH_LOGIN, GH_AUTH)
@@ -13,11 +15,10 @@ console.log('GH_AUTH', GH_AUTH)
     const newProject = await octoController.listProjectItems(TO_PROJECT)
     const toProjectId = newProject.user.projectV2.id
 
-    console.log('TO_PROJECT_ID', toProjectId)
     for (const FROM_PROJECT of FROM_PROJECTS) {
       const oldProject = await octoController.listProjectItems(FROM_PROJECT)
-      const fromProjectId = oldProject.user.projectV2.id
-      console.log('FROM_PROJECT_ID', fromProjectId)
+      console.log('TO_PROJECT_ID', newProject.user.projectV2.title)
+      console.log('FROM_PROJECT', oldProject.user.projectV2.title)
 
       const items = oldProject.user.projectV2.items.nodes
 
@@ -29,5 +30,6 @@ console.log('GH_AUTH', GH_AUTH)
         const number = item.addProjectV2ItemById.item.content.number
         console.log(`#${number} - ${title}`)
       }
+      console.log()
     }
   })()
